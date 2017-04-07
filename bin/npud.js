1ac7aae83f9fac3b0c75c591b3066467bf8a53a1
Dafrok <o.o@mug.dog>
2017-04-07 13:36:30 +0800

    Take a few change

--- bin/npud.js
+++ bin/npud.js
@@ -20,40 +20,37 @@ const packages = require('./packages')
 ~(async () => {
   cli.info('Fetching packages info...')
   let progress = 0
-  await Promise.all(packages.map(pack => 
-    fetch(`https://www.npmjs.com/package/${pack.name}`)
-      .then(res => res.text())
+  const result = await Promise.all(packages.map(pack => 
+    fetch(`https://www.npmjs.com/package/${pack.name}`).then(res => res.text())
       .then(text => {
         const $ = cheerio.load(text)
         pack.datetime = $('[data-date]').attr('data-date')
         cli.progress(++progress / packages.length)
         return Promise.resolve(pack)
       })))
-    .then(result => {
-      cli.ok('Done.')
-      result.sort((a, b) => {
-        switch (args.sort.toLowerCase()) {
-          case 'asc': return Date.parse(a.datetime) > Date.parse(b.datetime)
-          case 'desc': return Date.parse(a.datetime) < Date.parse(b.datetime)
-          default: return 0
-        }
-      })
+  cli.ok('Done.')
+  result.sort((a, b) => {
+    switch (args.sort.toLowerCase()) {
+      case 'asc': return Date.parse(a.datetime) > Date.parse(b.datetime)
+      case 'desc': return Date.parse(a.datetime) < Date.parse(b.datetime)
+      default: return 0
+    }
+  })
 
-      result.unshift({name: 'Name', datetime: 'Update Datetime', dependency: 'Type'})
-      const padLength = {}
-      result.forEach(item => {
-        for (let key in item) {
-          padLength[key] = Math.max(padLength[key] || 0, item[key].length)
-        }
-      })
+  result.unshift({name: 'Name', datetime: 'Update Datetime', dependency: 'Type'})
+  const padLength = {}
+  result.forEach(item => {
+    for (let key in item) {
+      padLength[key] = Math.max(padLength[key] || 0, item[key].length)
+    }
+  })
 
-      for (let i = 0; i < result.length; i++) {
-        const item = result[i]
-        if (i === 0) {
-          console.log(`${clc.blue(pad(item.name, padLength.name))}\t${clc.blue(pad(item.datetime, padLength.datetime))}\t${clc.blue(pad(item.dependency, padLength.dependency))}`)
-        } else {
-          console.log(`${pad(item.name, padLength.name)}\t${pad(i === 0 ? item.datetime : moment(item.datetime).format('MM-DD-YYYY hh:mm:ss'), padLength.datetime)}\t${pad(item.dependency, padLength.dependency)}`)
-        }
-      }
-    })
+  for (let i = 0; i < result.length; i++) {
+    const item = result[i]
+    if (i === 0) {
+      console.log(`${clc.blue(pad(item.name, padLength.name))}\t${clc.blue(pad(item.datetime, padLength.datetime))}\t${clc.blue(pad(item.dependency, padLength.dependency))}`)
+    } else {
+      console.log(`${pad(item.name, padLength.name)}\t${pad(i === 0 ? item.datetime : moment(item.datetime).format('MM-DD-YYYY hh:mm:ss'), padLength.datetime)}\t${pad(item.dependency, padLength.dependency)}`)
+    }
+  }
 })()
